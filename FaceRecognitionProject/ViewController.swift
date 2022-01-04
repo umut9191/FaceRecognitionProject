0b89d74df850f8765bcb4951d7c92668624800c1
umut9191 <umutsurmeli91@gmail.com>
2022-01-04 22:53:05 +0300

    Initial Commit

---
+++ FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by Mac on 4.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
